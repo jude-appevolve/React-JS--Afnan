d6ab276abd636e0f2e56b2bf90d359a5bb700540
afnan-1 <roman.nadeem@gmail.com>
2021-10-29 00:00:19 +0500

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  // const [value, setValue] = useState("");
+  // const django = () => {
+  //   axios.get("/api").then((data) => setValue(data.data));
+  // };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Frontend
+      {/* <button onClick={django}> Click me for backend connection</button> */}
+      {/* {value && value} */}
     </div>
   );
 }
